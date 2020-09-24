1ab1fa7aab83b001d28609e719888dd7c62ba43d
Romain PERRIN <romain.perrin@etu.unistra.fr>
2020-09-24 14:44:43 +0200

    adding Directory class

---
+++ src/fileSystemModel/Directory.java
@@ -0,0 +1,78 @@
+package fileSystemModel;
+
+import java.util.ArrayList;
+
+// Represents a directory containing both File and Directory (both SystemComponent)
+public class Directory implements SystemComponent
+{
+	
+	// attributes
+	public String name;
+	public SystemComponent parent;
+	public ArrayList<SystemComponent> children;
+	
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	
+	// constructor
+	public Directory(String name, SystemComponent parent)
+	{
+		this.name = name;
+		this.parent = parent;
+		this.children = new ArrayList<SystemComponent>();
+	}
+	
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+
+	@Override
+	public String getName()
+	{
+		return this.name;
+	}
+
+	@Override
+	public String getContent()
+	{
+		return null;
+	}
+
+	@Override
+	public SystemComponent getParent()
+	{
+		return this.parent;
+	}
+
+	@Override
+	public ArrayList<SystemComponent> getChildren()
+	{
+		return this.children;
+	}
+
+	@Override
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	@Override
+	public void setContent(String content)
+	{
+		return;
+	}
+
+	@Override
+	public void setParent(SystemComponent parent)
+	{
+		this.parent = parent;
+	}
+
+	@Override
+	public void setChildren(ArrayList<SystemComponent> children)
+	{
+		this.children = children;
+	}
+
+}
